520dc7c0acf116217c17ee1509a60431a872cf35
Joezari Borlongan <bor.joezari@gmail.com>
2026-04-09 09:29:55 -0700

    init

---
+++ src/test/java/com/joe_bor/svt_api/SvtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joe_bor.svt_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvtApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
