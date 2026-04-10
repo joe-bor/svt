838f2e438a21ce7e8715ad062bcdc0bcd3dcb386
Joezari Borlongan <bor.joezari@gmail.com>
2026-04-10 13:29:23 -0700

    feat(route): add location route lookup queries

--- src/main/java/com/joe_bor/svt_api/repositories/location/LocationRepository.java
+++ src/main/java/com/joe_bor/svt_api/repositories/location/LocationRepository.java
@@ -2,9 +2,14 @@ package com.joe_bor.svt_api.repositories.location;
 
 import com.joe_bor.svt_api.models.location.LocationEntity;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LocationRepository extends JpaRepository<LocationEntity, Long> {
+
+    Optional<LocationEntity> findByRouteOrder(Short routeOrder);
+
+    List<LocationEntity> findAllByBranchesFromOrderByIdAsc(LocationEntity branchesFrom);
 
     List<LocationEntity> findAllByOrderByDetourAscRouteOrderAscIdAsc();
 }
